612c1d2663b4f0e4e50cbde9997dd73ec498ac9a
i51chen <1959840525@qq.com>
2021-07-11 17:24:43 +0800

    1

--- practice1.py
+++ practice1.py
@@ -3,9 +3,16 @@ if __name__ == '__main__':
     b = True
     list1 = [a, b, a]
     list2 = list('123456')
-    print(any(list1))
-    print(all(list1))
-    print(list2)
-    output = "".join(list2)
-    print(output)
-
+    # print(any(list1))
+    # print(all(list1))
+    # print(list2)
+    # output = "".join(list2)
+    # print(output)
+    #
+    # if b:
+    #     print(1)
+    #
+    eval_str = 'if b:' \
+               'print(1)'
+    eval(eval_str)
+    # exec()
